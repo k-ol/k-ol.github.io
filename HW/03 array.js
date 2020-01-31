1b0acf9c9883eb72bc4f6d18a896c5b859372210
Oleksandr Kuts <kuts.ol.vas@gmail.com>
2020-01-31 14:00:08 +0200

    Home work

---
+++ HW/03 array.js
@@ -0,0 +1,12 @@
+// Пользователь вводит три предложения которые добавляются в массив
+// Этот массив соединяется со вторым массивом
+// Вывести общее количество символов в финальном массиве
+var arrSentence = [],
+    arr2 = [1,5,true,NaN,'try',undefined,[1,2,[3,true],8,'mother, father'],'mother, father'];
+arrSentence[arrSentence.length] = prompt('Введите предложение -' + (arrSentence.length + 1),'');
+arrSentence[arrSentence.length] = prompt('Введите предложение -' + (arrSentence.length + 1),'');
+arrSentence[arrSentence.length] = prompt('Введите предложение -' + (arrSentence.length + 1),'');
+arrSentence = arrSentence.concat(arr2);  
+arrSentence = arrSentence.flat(Infinity);
+result = arrSentence.join('');
+console.log(result.length);
