17ccbe3f8f9ed0f8e3db15bf71351d1ae2218fe6
Oleksandr Kuts <kuts.ol.vas@gmail.com>
2020-02-02 08:38:25 +0200

    Update 03 additional_1.js
    Element

--- HW/03 additional_1.js
+++ HW/03 additional_1.js
@@ -1,3 +1,3 @@
 var arr = [NaN, 0, 15, false, -22, '',undefined, 47, null];
-var result = arr.filter(arr => Boolean(arr));
+var result = arr.filter(element => Boolean(element));
 console.log(result);
