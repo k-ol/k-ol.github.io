06bf156b6731ca867c6d27a5b12c8d2166ed46fe
Oleksandr Kuts <kuts.ol.vas@gmail.com>
2020-02-07 15:02:10 +0200

    HW5

--- HW/05 add.js
+++ HW/05 add.js
@@ -1,21 +1,21 @@
 // Как бонус: сделайте оптимально и игру, которая закончится только после нажатия отмена
 var compStep, userStep,
     list = ['камень','ножницы','бумага'],
-    model =[[ 0,-1, 1], [ 1, 0,-1], [-1, 1, 0]];
+    model =[[ 2, 1, 3], [ 3, 2, 1], [ 1, 3, 2]];
 for (;true;) {
-    compStep = Math.floor(Math.random()/0.33333);
-    userStep = prompt('Ваш ход:\nкамень - 0\nножницы - 1\nбумага - 2\nВыход - Отмена','');
-    if (!userStep) {
+    compStep = Math.floor(Math.random()/(1/3));
+    userStep = prompt('Ваш ход:\nкамень - 1\nножницы - 2\nбумага - 3\nВыход - Отмена','');
+    if (typeof(userStep) != 'string') {
       alert('Пока');
       break;
     } else {
-        userStep = Number(userStep);
+        userStep = Number(userStep-1);
         switch (model[compStep][userStep]) {
-          case 1: alert('Победа - Я: '+ list[compStep]+' / Вы - '+list[userStep]);
+          case 3: alert('Победа - Я: '+ list[compStep]+' / Вы - '+list[userStep]);
             break;
-          case 0: alert('Ничья - оба: '+ list[compStep]);
+          case 2: alert('Ничья - оба: '+ list[compStep]);
             break;    
-          case -1: alert('Поражение - Я: '+ list[compStep]+' / Вы - '+list[userStep]);
+          case 1: alert('Поражение - Я: '+ list[compStep]+' / Вы - '+list[userStep]);
             break;         
           default : alert('Некоректные данные');
         } 
--- HW/05 stone.js
+++ HW/05 stone.js
@@ -5,19 +5,19 @@
 // Проверки на отмену и некорректные данные тоже нужны.
 var compStep, userStep,
     list = ['камень','ножницы','бумага'],
-    model =[[ 0,-1, 1], [ 1, 0,-1], [-1, 1, 0]];
-    compStep = Math.floor(Math.random()/0.33333);
-    userStep = prompt('Ваш ход:\nкамень - 0\nножницы - 1\nбумага - 2','');
-    if (!userStep) {
+    model =[[2, 1, 3], [3, 2, 1], [1, 3, 2]];
+    compStep = Math.floor(Math.random()/(1/3));
+    userStep = prompt('Ваш ход:\nкамень - 1\nножницы - 2\nбумага - 3','');
+    if (typeof(userStep) != 'string') {
       alert('Пока');
     } else {
-        userStep = Number(userStep);
+        userStep = Number(userStep-1);
         switch (model[compStep][userStep]) {
-          case 1: alert('Победа - Я: '+ list[compStep]+' / Вы - '+list[userStep]);
+          case 3: alert('Победа - Я: '+ list[compStep]+' / Вы - '+list[userStep]);
             break;
-          case 0: alert('Ничья - оба: '+ list[compStep]);
+          case 2: alert('Ничья - оба: '+ list[compStep]);
             break;    
-          case -1: alert('Поражение - Я: '+ list[compStep]+' / Вы - '+list[userStep]);
+          case 1: alert('Поражение - Я: '+ list[compStep]+' / Вы - '+list[userStep]);
             break;         
           default : alert('Некоректные данные');
         } 
