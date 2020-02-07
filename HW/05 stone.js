1f1fb6de613375343fdbd09dd7c1b5625e94eefd
Oleksandr Kuts <kuts.ol.vas@gmail.com>
2020-02-07 18:51:59 +0200

    Табуляция

--- HW/05 stone.js
+++ HW/05 stone.js
@@ -8,17 +8,17 @@ var compStep, userStep,
     model =[[2, 1, 3], [3, 2, 1], [1, 3, 2]];
     compStep = Math.floor(Math.random()/(1/3));
     userStep = prompt('Ваш ход:\nкамень - 1\nножницы - 2\nбумага - 3','');
-    if (typeof(userStep) != 'string') {
-      alert('Пока');
-    } else {
-        userStep = Number(userStep-1);
-        switch (model[compStep][userStep]) {
-          case 3: alert('Победа - Я: '+ list[compStep]+' / Вы - '+list[userStep]);
-            break;
-          case 2: alert('Ничья - оба: '+ list[compStep]);
-            break;    
-          case 1: alert('Поражение - Я: '+ list[compStep]+' / Вы - '+list[userStep]);
-            break;         
-          default : alert('Некоректные данные');
-        } 
-    }
+if (typeof(userStep) != 'string') {
+  alert('Пока');
+} else {
+    userStep = Number(userStep-1);
+    switch (model[compStep][userStep]) {
+      case 3: alert('Победа - Я: '+ list[compStep]+' / Вы - '+list[userStep]);
+        break;
+      case 2: alert('Ничья - оба: '+ list[compStep]);
+        break;    
+      case 1: alert('Поражение - Я: '+ list[compStep]+' / Вы - '+list[userStep]);
+        break;         
+      default : alert('Некоректные данные');
+    } 
+}
