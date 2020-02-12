fa540797b110ca6679aa669e8fbe97093b89070f
Oleksandr Kuts <kuts.ol.vas@gmail.com>
2020-02-12 16:37:28 +0200

    Error

--- HW/06 3 changeN.js
+++ HW/06 3 changeN.js
@@ -1,6 +1,6 @@
 // 3) Найти и заменить в тексте: число 1 на one, 2 на two, 3 на three
 
-var text ='1 3 Lorem 1 ipsum dolor 3 sit amet 2, 2.2 consectetur 3adipisicing 1elit1. 33 Distinctio 1. vol1uptate 11optio in labore 22fuga aliquam33 officia 2';
+var text ='1 3 Lorem 1 ipsum dolor 3 sit amet 2, 2.2 consectetur 3adipisicing 1elit1. 33 Distinctio 1. vol1uptate 11optio in labore 22fuga aliquam33 officia 2.2';
 console.log(text);
 var words = ['one', 'two', 'three'];
 var arrText = text.split(' ');
@@ -12,7 +12,7 @@ for (var i = 1 ; i < 4 ; i++) {
             arrText[j] = arrText[j].replace(String(i), words[i-1]);            
         } else if (arrText.slice(0,4) === i + '. ') {
             arrText[j] = arrText[j].replace(String(i), words[i-1]);
-        }
+        } 
    }
 }
 text = arrText.join(' ') 
