83399eb4b6f7730758cab6714a061658b8cdd69f
Oleksandr Kuts <kuts.ol.vas@gmail.com>
2020-02-21 20:37:46 +0200

    Убрал 2 for in

--- HW/09 copy object.js
+++ HW/09 copy object.js
@@ -20,12 +20,8 @@ let myObj2 = {
 function comparisonObj(obj1,obj2){
     let rezObj ={};
     for (let elObj1 in obj1) {
-        if (elObj1 in obj2) {  // есть ли ключ в объекте 2 (зря не ходить по объекту)
-            for (let elObj2 in obj2) {
-                if (elObj1 === elObj2 && obj1[elObj1] === obj2[elObj2]){
+        if (elObj1 in obj2 && obj1[elObj1] === obj2[elObj1]) {  
                     rezObj[obj1[elObj1]] = elObj1;
-                }
-            }
         }
     }
     return rezObj;
