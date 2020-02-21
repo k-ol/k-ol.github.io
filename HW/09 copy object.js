5874e34c8ce00b24493a56f7dc78333bddc0c26c
Oleksandr Kuts <kuts.ol.vas@gmail.com>
2020-02-21 20:30:20 +0200

    есть ли ключ в объекте 2

--- HW/09 copy object.js
+++ HW/09 copy object.js
@@ -19,10 +19,12 @@ let myObj2 = {
 
 function comparisonObj(obj1,obj2){
     let rezObj ={};
-    for (let el_obj1 in obj1) {
-        for (let el_obj2 in obj2){
-            if (el_obj1 === el_obj2 && obj1[el_obj1] === obj2[el_obj2]){
-                rezObj[obj1[el_obj1]] = el_obj1;
+    for (let elObj1 in obj1) {
+        if (elObj1 in obj2) {  // есть ли ключ в объекте 2 (зря не ходить по объекту)
+            for (let elObj2 in obj2) {
+                if (elObj1 === elObj2 && obj1[elObj1] === obj2[elObj2]){
+                    rezObj[obj1[elObj1]] = elObj1;
+                }
             }
         }
     }
