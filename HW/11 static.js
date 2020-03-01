20297c0d2f674772bb0dc0cd20e2e1b5f8021f01
Oleksandr Kuts <kuts.ol.vas@gmail.com>
2020-03-01 15:32:23 +0200

    Static method

---
+++ HW/11 static.js
@@ -0,0 +1,28 @@
+// напишите 3 конструктора. В первом конструкторе есть три метода: 
+// run, go, say. Во втором два метода: run,go, но с другим функционалом. 
+// В третьем конструкторе только статический метод, который создает 
+// объекты из конструктора, имя которого указано в параметрах.
+
+function Constr1() {
+}
+Constr1.prototype.run = function(){return 'run'};
+Constr1.prototype.go  = function(){return 'go'};
+Constr1.prototype.say = function(){return 'say'};
+
+function Constr2() {
+}
+Constr2.prototype.run = function(){return 'run-run'};
+Constr2.prototype.go  = function(){return 'go-go'};
+
+function Constr3(){};
+Constr3.crObj = function(nameConstr) {
+    return Object.create(nameConstr.prototype);
+}
+
+let obj1 = Constr3.crObj(Constr1);
+let obj2 = Constr3.crObj(Constr2);
+console.log(obj1.run());
+console.log(obj2.run());
+console.log(obj1);
+console.log(obj2);
+
