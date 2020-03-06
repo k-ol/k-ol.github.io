440a9bd75426039c9496f4aad96e7a722eb1ac4c
Oleksandr Kuts <kuts.ol.vas@gmail.com>
2020-03-06 06:07:21 +0200

    Наследование

--- HW/11 static.js
+++ HW/11 static.js
@@ -3,22 +3,28 @@
 // В третьем конструкторе только статический метод, который создает 
 // объекты из конструктора, имя которого указано в параметрах.
 
-function Constr1() {};
-Constr1.prototype.run = function(){return 'run'};
-Constr1.prototype.go  = function(){return 'go'};
-Constr1.prototype.say = function(){return 'say'};
+
 
 function Constr2() {};
-Constr2.prototype.run = function(){return 'run-run'};
-Constr2.prototype.go  = function(){return 'go-go'};
+Constr2.prototype.run = function(){return 'run'};
+Constr2.prototype.go  = function(){return 'go'};
 
-function Constr3(){};
-Constr3.crObj = function(nameConstr) {
-    let obj = Object.create(nameConstr.prototype);// или return new nameConstr();
-    nameConstr.prototype.constructor = Constr3;
+
+function Constr1() {};
+Constr1.prototype = Object.create(Constr2.prototype);
+Constr1.prototype.constructor = Constr1;
+Constr1.prototype.say = function(){return 'say-say'}; 
+Constr1.prototype.run = function(){return 'run-run'}; 
+Constr1.prototype.go  = function(){return 'go-go'};   
+
+function Constr3(){}; // конструктор 3
+Constr3.crObj = function(nameConstr) { 
+    let obj = Object.create(nameConstr.prototype);
+    nameConstr.prototype.constructor = Constr3; 
     return obj;
 }
 
+// проверка работы
 let obj1 = Constr3.crObj(Constr1);
 let obj2 = Constr3.crObj(Constr2);
 console.log(obj1.run());
